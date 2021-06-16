f09df58e94d5155d4f55cc480e100977bfeb9af5
402717854 <402717854@qq.com>
2021-06-16 16:42:40 +0800

    develop

---
+++ springcloud-alibaba-nacos-consumer/src/test/java/com/springcloud/alibaba/demo/SpringcloudAlibabaNacosConsumerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.springcloud.alibaba.demo;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudAlibabaNacosConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ springcloud-alibaba-nacos-provider/src/test/java/com/springcloud/alibaba/demo/SpringcloudAlibabNacosProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.alibaba.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudAlibabNacosProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
