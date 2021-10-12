11a77939064ef99615be4f2e7440f684553457a0
402717854 <402717854@qq.com>
2021-10-12 20:31:46 +0800

    gateway基于 Hystrix 实现服务容错

---
+++ gateway-group/gateway-hystrix-demo/src/main/java/com/springcloud/alibaba/demo/GatewayHystrixDemoApplication.java
@@ -0,0 +1,11 @@
+package com.springcloud.alibaba.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayHystrixDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayHystrixDemoApplication.class, args);
+    }
+}
