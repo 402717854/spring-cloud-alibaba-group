a86b7a7ba2b754229d9abc8df57ef8e0c2b338e8
402717854 <402717854@qq.com>
2021-10-24 17:56:27 +0800

    nacos服务实例实现不停机热部署以及灰度发布实战

---
+++ springcloud-ribbon-group/ribbon-consumer-demo4/src/main/java/com/springcloud/alibaba/demo/RibbonConsumerDemo4Application.java
@@ -0,0 +1,11 @@
+package com.springcloud.alibaba.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonConsumerDemo4Application {
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonConsumerDemo4Application.class, args);
+    }
+}
